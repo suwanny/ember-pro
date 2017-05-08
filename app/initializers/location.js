9b618f279d4734b73fff232f86163cdc17eea238
Mike North <michael.l.north@gmail.com>
2017-05-08 14:04:51 +0200

    exercise 3 - broccoli

--- app/initializers/location.js
+++ app/initializers/location.js
@@ -1,19 +1,19 @@
 // @ts-check
 
 export function initialize(app) {
-  let { geolocation } = navigator;
-  app.deferReadiness();
-  geolocation.getCurrentPosition((pos) => {
-    let {
-      coords: { latitude, longitude }
-    } = pos;
-    app.register(
-      'data:location',
-      { latitude, longitude },
-      { instantiate: false }
-    );
-    app.advanceReadiness();
-  });
+  // let { geolocation } = navigator;
+  // app.deferReadiness();
+  // geolocation.getCurrentPosition((pos) => {
+  //   let {
+  //     coords: { latitude, longitude }
+  //   } = pos;
+  //   app.register(
+  //     'data:location',
+  //     { latitude, longitude },
+  //     { instantiate: false }
+  //   );
+  //   app.advanceReadiness();
+  // });
   // application.inject('route', 'store', 'service:store');
 }
 
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,24 @@
 /* eslint-env node */
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
+const Filter = require('broccoli-filter');
+
+function CommentFilter(inputNode) {
+  Filter.call(this, inputNode);
+  // this.extensions = operateOn;
+  // this.targetExtension = transformTo;
+}
+
+CommentFilter.prototype = Object.create(Filter.prototype);
+
+CommentFilter.prototype.processString = function(existingString) {
+  return `/** (c) 2017 Mike ${(new Date()).toISOString()} */\n ${existingString}  `;
+};
+
+CommentFilter.prototype.extensions = ['js', 'ts', 'css'];
+// CommentFilter.prototype.targetExtension = 'js';
+
+
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
@@ -8,18 +26,5 @@ module.exports = function(defaults) {
 
   app.import('vendor/math-shim.js');
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-
-  return app.toTree();
+  return new CommentFilter(app.toTree());
 };
