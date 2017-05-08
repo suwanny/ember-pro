7141f816bd1782c75a13cb497d1e87f667bce839
Mike North <michael.l.north@gmail.com>
2017-05-08 15:46:19 +0200

    exercise 6 - fastboot

--- app/initializers/location.js
+++ app/initializers/location.js
@@ -1,19 +1,27 @@
 // @ts-check
 
 export function initialize(app) {
-  // let { geolocation } = navigator;
-  // app.deferReadiness();
-  // geolocation.getCurrentPosition((pos) => {
-  //   let {
-  //     coords: { latitude, longitude }
-  //   } = pos;
-  //   app.register(
-  //     'data:location',
-  //     { latitude, longitude },
-  //     { instantiate: false }
-  //   );
-  //   app.advanceReadiness();
-  // });
+  if (typeof FastBoot === 'undefined') {
+    let { geolocation } = navigator;
+    app.deferReadiness();
+    geolocation.getCurrentPosition((pos) => {
+      let {
+        coords: { latitude, longitude }
+      } = pos;
+      app.register(
+        'data:location',
+        { latitude, longitude },
+        { instantiate: false }
+      );
+      app.advanceReadiness();
+    });
+  } else {
+    app.register(
+      'data:location',
+      { latitude: -1, longitude: -1 },
+      { instantiate: false }
+    );
+  }
   // application.inject('route', 'store', 'service:store');
 }
 
