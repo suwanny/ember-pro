dbb4bfadf1c949d577f9d4ba80003efff02b759f
Mike North <michael.l.north@gmail.com>
2017-05-09 14:12:34 +0200

    Setup for persisted state exercise

--- app/components/post-full.ts
+++ app/components/post-full.ts
@@ -1,9 +1,20 @@
 import Ember from 'ember';
 import stateFor from 'ember-state-services/state-for';
+import Comment from '../models/comment';
 
 const { Component } = Ember;
 
 export default Component.extend({
   classNames: ['post-full'],
-  tempState: stateFor('temp', 'model')
+  tempState: stateFor('temp', 'model'),
+  commentDraft: stateFor('comment-draft', 'model'),
+  actions: {
+    foo() {
+      let wm = stateFor('comment-draft');
+      console.log('The weakmap is', wm);
+    },
+    deleteTheComment(comment) {
+      alert(comment.get('body'));
+    }
+  }
 });
---
+++ app/states/comment-draft.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+
+const { Object: EObj } = Ember;
+
+const CommentDraft = EObj.extend({
+
+});
+
+CommentDraft.reopenClass({
+  initialState(post) {
+    return {
+      postId: post.id,
+      body: ''
+    };
+  }
+});
+
+export default CommentDraft;
+
+
+// What's passed to the post-comment component
+
+// function deleteThenComment() {}
+
+// typical action
+// let a = function(comment) {
+//   return deleteTheComment(comment); 
+// };
+
+// let b = function(comment) { // defined in post-full
+//   return function() {
+//     deleteThenComment(comment);
+//   };
+// };
+
+// console.log(a, b);
