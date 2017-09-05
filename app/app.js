89494f7ff8d4edb206e8303f893fede018423533
Michael North <michael.l.north@gmail.com>
2017-09-05 12:28:16 -0700

    Exercise 1

--- app/app.js
+++ app/app.js
@@ -1,9 +1,17 @@
 import Ember from 'ember';
 import Resolver from './resolver';
+/* globals define: true*/
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
 const { Application } = Ember;
+
+define('math', [], function() {
+  return {
+    default: Math,
+    PI: Math.PI
+  };
+});
 
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import { default as math, PI } from 'math';
 
+math.sqrt(4); // 2
+console.log(PI); // 3.145926...
 const { Route, inject } = Ember;
 
 export default Route.extend(ApplicationRouteMixin, {
