9c80db83afd4621709ac44f6a4b7ec2d8f587941
Michael North <michael.l.north@gmail.com>
2017-09-05 13:38:08 -0700

    Exercise 2: initializers

--- app/controllers/application.ts
+++ app/controllers/application.ts
@@ -1,7 +1,13 @@
 import Ember from 'ember';
 
-const { Controller, inject } = Ember;
+const { Controller, inject, computed } = Ember;
 
 export default Controller.extend({
+  init() {
+    this._super(...arguments);
+    let prop = Ember.getOwner(this).lookup('data:location');
+    // this.loc = 
+    this.set('loc', prop);
+  },
   currentUser: inject.service()
 });
