d866fbea8f160bbde433a83fc457c5f66ea621f9
Michael North <michael.l.north@gmail.com>
2017-11-02 10:43:12 -0700

    fastboot initial setup

--- app/initializers/geo.js
+++ app/initializers/geo.js
@@ -1,13 +1,19 @@
-export function initialize(app) {
-  app.deferReadiness();
-  const { geolocation } = navigator;
-  geolocation.getCurrentPosition((pos) => {
-    let { coords: { latitude: lat, longitude: lng } } = pos;
-    app.register('data:location', { lat, lng }, {
-      instantiate: false
+function attemptLocationRetrieval(app) {
+  if (typeof FastBoot === 'undefined') {
+    app.deferReadiness();
+    const { geolocation } = navigator;
+    geolocation.getCurrentPosition((pos) => {
+      let { coords: { latitude: lat, longitude: lng } } = pos;
+      app.register('data:location', { lat, lng }, {
+        instantiate: false
+      });
+      app.advanceReadiness();
     });
-    app.advanceReadiness();
-  });
+  }
+}
+
+export function initialize(app) {
+  attemptLocationRetrieval(app);
 }
 
 export default {
