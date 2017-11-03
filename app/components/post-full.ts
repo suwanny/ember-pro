87460333771f75d961f52c9f2b5971090dbcc53c
Michael North <michael.l.north@gmail.com>
2017-11-03 10:32:54 -0700

    draft state

--- app/components/post-full.ts
+++ app/components/post-full.ts
@@ -1,7 +1,10 @@
 import Ember from 'ember';
+import stateFor from 'ember-state-services/state-for';
 
 const { Component } = Ember;
 
 export default Component.extend({
-  classNames: ['post-full']
+  draftComment: '',
+  classNames: ['post-full'],
+  postInfo: stateFor('post-info', 'model')
 });
--- app/initializers/geo.js
+++ app/initializers/geo.js
@@ -1,14 +1,14 @@
 function attemptLocationRetrieval(app) {
   if (typeof FastBoot === 'undefined') {
-    app.deferReadiness();
-    const { geolocation } = navigator;
-    geolocation.getCurrentPosition((pos) => {
-      let { coords: { latitude: lat, longitude: lng } } = pos;
-      app.register('data:location', { lat, lng }, {
-        instantiate: false
-      });
-      app.advanceReadiness();
-    });
+    // app.deferReadiness();
+    // const { geolocation } = navigator;
+    // geolocation.getCurrentPosition((pos) => {
+    //   let { coords: { latitude: lat, longitude: lng } } = pos;
+    //   app.register('data:location', { lat, lng }, {
+    //     instantiate: false
+    //   });
+    //   app.advanceReadiness();
+    // });
   }
 }
 
