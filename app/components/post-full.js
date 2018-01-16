c1b968a14f10565874d1ff5c5be192c70e916951
Michael North <michael.l.north@gmail.com>
2018-01-16 13:29:19 -0800

    starting point for draft state

--- app/components/post-full.js
+++ app/components/post-full.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
+import stateFor from 'ember-state-services/state-for';
 
 const { Component } = Ember;
 
 export default Component.extend({
+  info: stateFor('post-info', 'model'),
   classNames: ['post-full']
 });
