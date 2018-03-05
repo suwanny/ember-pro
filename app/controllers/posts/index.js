6e13dd13b584cf87f88aa6d71cdff4f7a51bec0d
Soo Hwan Park <sopark@linkedin.com>
2018-03-05 12:44:12 -0800

    add data to posts controller

---
+++ app/controllers/posts/index.js
@@ -0,0 +1,14 @@
+import Controller from '@ember/controller';
+import { getOwner } from '@ember/application';
+
+export default Controller.extend({
+  init() {
+    this._super(...arguments);
+
+    const container = getOwner(this);
+    const loc = container.lookup("data:location");
+
+    console.log('loc from posts controller .. ', loc);
+    this.set('loc', loc);
+  }
+});
