a4f2eeebf01aff5f0db6302c3574f8479afce73f
Soo Hwan Park <sopark@linkedin.com>
2018-03-06 10:56:30 -0800

    exercise 07

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,6 +4,7 @@ import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mi
 const { Route, inject, getOwner } = Ember;
 
 export default Route.extend(ApplicationRouteMixin, {
+  // fastboot: inject.service(),
   session: inject.service(),
   currentUser: inject.service(),
   actions: {
@@ -16,5 +17,28 @@ export default Route.extend(ApplicationRouteMixin, {
     if (this.get('session.isAuthenticated')) {
       return this.get('currentUser').loadUserInfo();
     }
-  }
+  },
+  setupController(controller) {
+    this._super(...arguments);
+
+    let reqObj = getOwner(this).lookup("data:request");
+    controller.set("req", reqObj);
+
+
+    // let headers = this.get("fastboot.request.headers");
+    // let shoebox = this.get("fastboot.shoebox");
+    // let shoeboxStore = shoebox.retrieve("my-store");
+
+    // if (this.get('fastboot.isFastBoot')) {
+    //   if (!shoeboxStore) {
+    //     // Lazily create the store
+    //     shoeboxStore = {};
+    //     shoebox.put('my-store', shoeboxStore);
+    //   }
+
+    //   //  Put the data in the store
+    //   let xRequestHeader = headers.get("X-Request");
+    //   shoeboxStore['data:request'] = xRequestHeader;
+    // }
+  },
 });
